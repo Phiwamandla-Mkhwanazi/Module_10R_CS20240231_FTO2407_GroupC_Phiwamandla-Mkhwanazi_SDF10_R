2d9229799d3de40d5e5db334cfeb9aca0f31c41e
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2024-08-07 09:49:58 +0200

    update

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+"use strict";
+
 //Declare and Initialize variables
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getDatabase, ref , push} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
 const inputFieldEl = document.getElementById("input-field");
 const itemListEl = document.getElementById("item-list");
 const addButtonEl = document.getElementById("add-button");
 
 
 /*Database Connection - Start*/
-const firebaseConfig = 
-{   //Connection String
-    databaseURL : "https://shopping-cart-codecat-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+ const firebaseConfig = 
+  {   
+      databaseURL : "https://shopping-cart-codecat-default-rtdb.europe-west1.firebasedatabase.app/"
+  }
 
-const app = initializeApp(firebaseConfig.databaseURL);
+const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-const referenceInDB = ref(database, "shopping")
-console.log(firebaseConfig.databaseURL)
+const referenceInDB = ref(database, "Shopping Cart")
 /*Database Connection - End*/ 
 
 
@@ -23,8 +24,8 @@ console.log(firebaseConfig.databaseURL)
 // Event listener for the 'Add to cart' button
 addButtonEl.addEventListener("click", function() 
 {
-    let inputValue = inputFieldEl.value.trim(); // Trim to remove leading/trailing whitespace
-    console.log(inputValue); // Log the input value to the console
+    let inputValue = inputFieldEl.value.trim(); 
+    console.log(inputValue)
     push(referenceInDB,inputValue)// Add Items to the database
     if (inputValue !== "") 
     { // Check if the input is not empty
